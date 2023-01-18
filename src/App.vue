bc62e5fa6c218ab4d9c0f26af525481a161e18e2
olatunji1985 <profteejee@yahoo.co.uk>
2023-01-18 04:20:46 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <Header title="My Cool Vue App" />
+        <button>Add Task</button>
+    </div>
+</template>
+
+<script>
+import Header from "./Header.vue"
+export default{
+    name:'App',
+    components: {
+        Header
+    }
+}
+
+</script>
+
+<style>
+#app {
+    text-align: center;
+    color: blue;
+    margin-top: 60px;
+}
+button {
+    padding: 10px 20px;
+    font-size: 15px;
+    background: blueviolet;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+</style>
---
+++ src/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <hi>{{ title }}</hi>
+    </header>
+</template>
+
+<script>
+export default{
+    name:'Header',
+    props: {
+        title:string
+    
+    }
+}
+</script>
+
+<style>
+</style>
